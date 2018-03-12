563ada18771da8a247e0370310cd482b76bc2650
Dakota Edwards <drakoden2000@gmail.com>
2018-03-12 09:59:08 -0700

    Homework breakpoint

--- Letter.js
+++ Letter.js
@@ -1,30 +1,36 @@
-function Letter(letter){
+function Letter(letter, original) {
     this.letter = letter,
-    this.guess = false,
-    this.letterSelect = function() {
-        if(this.guess === false) {
-            console.log("here")
-            this.letter = "_ "
-        }
-        else {
-            console.log(this.letter);
+        this.guess = false,
+        this.original = original,
+        this.letterSelect = function () {
+            if (this.guess === false) {
+                console.log("here")
+                this.letter = "_ "
+                console.log(this.letter)
+            }
+            else {
+                this.letter = this.original;
+                console.log(this.original)
+            }
+
         }
 
-    }
-
-    this.guessTrue = function(a) {
-        if (a === this.letter) {
+    this.guessTrue = function (a) {
+        if (a === this.original) {
             this.guess = true;
             console.log("there");
 
             this.letterSelect();
 
         }
-        console.log("here?")
+        else {
+            console.log("here?")
+            console.log(this.letter)
+        }
     }
 }
 
-var A = new Letter("A");
+var A = new Letter("A", "A");
 
 A.letterSelect();
 var a = "A";
---
+++ Word.js
@@ -0,0 +1,7 @@
+var Letter = require("./letter.js")
+
+function Word(word) {
+    this.wordArr = word,
+
+
+}
